13349ac356ef8bfefff46c7fc9d4462f3a9f1221
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-06-12 18:28:59 +0300

    The third homework is done

---
+++ HomeWorks_3/HomeWorks_3/Program.cs
@@ -0,0 +1,32 @@
+﻿
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        bool isPrime = IsPrimeNumber(number);
+
+        if (isPrime)
+        {
+            Console.WriteLine($"{number} is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is NOT a prime number.");
+        }
+    
+
+    static bool IsPrimeNumber(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
