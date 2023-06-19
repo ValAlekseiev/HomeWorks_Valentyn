f2079ed6a52515e69b84eb91118a74c1accab736
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-06-19 19:28:35 +0300

    two homework tasks are done

---
+++ HomeWork_4.1/HomeWork_4.1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+        int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+        Console.WriteLine("Enter the number to search for:");
+        int target = Convert.ToInt32(Console.ReadLine());
+
+        int position = Array.IndexOf(numbers, target);
+        if (position != -1)
+        {
+            Console.WriteLine($"Number {target} found at the position {position} in the array.");
+        }
+        else
+        {
+            Console.WriteLine($"Number {target} is not in the array.");
+        }
+   
---
+++ HomeWork_4.2/HomeWork_4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+        Console.WriteLine("The initial array of numbers:");
+        PrintArray(numbers);
+
+        ReverseArray(numbers);
+
+        Console.WriteLine("An array of numbers after reflection:");
+        PrintArray(numbers);
+    
+
+    static void ReverseArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
