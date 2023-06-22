27b421b454077571eced8425ce7c160268847069
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-06-22 19:28:47 +0300

    Homework lessons 6

---
+++ HomeWork_Leson 6/HomeWork_Leson 6/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number N of the prime number:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int primeNumber = FindNthPrime(n);
+
+        Console.WriteLine($"N-The simple number: {primeNumber}");
+    }
+
+    static int FindNthPrime(int n)
+    {
+        if (n <= 0)
+        {
+            throw new ArgumentException("The number N must be a positive number.");
+        }
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n)
+        {
+            if (IsPrime(number))
+            {
+                count++;
+            }
+
+            number++;
+        }
+
+        return number - 1;
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
