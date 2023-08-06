718ca828fce8f8202fba8b2daf246b764e8774e6
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-08-06 09:13:45 +0300

    HomeWork18

---
+++ HomeWork18/HomeWork18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an arithmetic expression: ");
+            string expression = Console.ReadLine();
+
+            try
+            {
+                double result = Calculate(expression);
+                Console.WriteLine($"result: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"error: {ex.Message}");
+            }
+        }
+
+        static double Calculate(string expression)
+        {
+            DataTable dt = new DataTable();
+            return Convert.ToDouble(dt.Compute(expression, ""));
+        }
+    }
+}
