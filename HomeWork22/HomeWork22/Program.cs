7a6a211cb95fa3a034371b6a5759a04a0538d882
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-09-07 19:25:01 +0300

    HomeWork22

---
+++ HomeWork22/HomeWork22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string logFilePath = "sample.log"; 
+
+       
+            if (!File.Exists(logFilePath))
+            {
+                Console.WriteLine("The log file does not exist.");
+                return;
+            }
+
+        
+            string[] logLines = File.ReadAllLines(logFilePath);
+
+         
+            int totalLogEntries = logLines.Length;
+            int errorCount = logLines.Count(line => line.Contains("ERROR"));
+            int warningCount = logLines.Count(line => line.Contains("WARNING"));
+            int infoCount = logLines.Count(line => line.Contains("INFO"));
+
+           
+            string report = $"Log File Analysis Report:\n" +
+                            $"Total Log Entries: {totalLogEntries}\n" +
+                            $"Errors: {errorCount}\n" +
+                            $"Warnings: {warningCount}\n" +
+                            $"Informational Entries: {infoCount}\n";
+
+            
+            Console.WriteLine(report);
+
+            string reportFilePath = "log_report.txt";
+            File.WriteAllText(reportFilePath, report);
+
+            Console.WriteLine($"Report written to {reportFilePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
+
