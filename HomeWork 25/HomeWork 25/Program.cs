94a11b4f8056b5ebdc734964facda11d896c953c
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-09-18 20:37:04 +0300

    HomeWork25 1-2

---
+++ HomeWork 25/HomeWork 25/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static object lockObject = new object();
+    static int currentNumber = 1;
+    static int maxNumbers = 10;
+
+    static void Main()
+    {
+        Thread evenThread = new Thread(PrintEvenNumbers);
+        Thread oddThread = new Thread(PrintOddNumbers);
+
+        evenThread.Start();
+        oddThread.Start();
+
+        evenThread.Join();
+        oddThread.Join();
+    }
+
+    static void PrintEvenNumbers()
+    {
+        while (currentNumber <= maxNumbers)
+        {
+            lock (lockObject)
+            {
+                if (currentNumber % 2 == 0)
+                {
+                    Console.WriteLine($"Even: {currentNumber}");
+                    currentNumber++;
+                }
+            }
+        }
+    }
+
+    static void PrintOddNumbers()
+    {
+        while (currentNumber <= maxNumbers)
+        {
+            lock (lockObject)
+            {
+                if (currentNumber % 2 != 0)
+                {
+                    Console.WriteLine($"Odd: {currentNumber}");
+                    currentNumber++;
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork 25_2/HomeWork 25_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static object lockA = new object();
+    static object lockB = new object();
+    static object lockC = new object();
+    static int maxIterations = 5;
+
+    static void Main()
+    {
+        Thread threadA = new Thread(PrintA);
+        Thread threadB = new Thread(PrintB);
+        Thread threadC = new Thread(PrintC);
+
+        threadA.Start();
+        threadB.Start();
+        threadC.Start();
+
+        threadA.Join();
+        threadB.Join();
+        threadC.Join();
+    }
+
+    static void PrintA()
+    {
+        for (int i = 0; i < maxIterations; i++)
+        {
+            lock (lockA)
+            {
+                Console.WriteLine("A");
+            }
+            Thread.Sleep(100); 
+        }
+    }
+
+    static void PrintB()
+    {
+        for (int i = 0; i < maxIterations; i++)
+        {
+            lock (lockA)
+            {
+                lock (lockB)
+                {
+                    Console.WriteLine("B");
+                }
+            }
+            Thread.Sleep(100); 
+        }
+    }
+
+    static void PrintC()
+    {
+        for (int i = 0; i < maxIterations; i++)
+        {
+            lock (lockB)
+            {
+                lock (lockC)
+                {
+                    Console.WriteLine("C");
+                }
+            }
+            Thread.Sleep(100); 
+        }
+    }
+}
