0829f871d7008b85cbaafa625f2907d9464cef2f
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-09-25 19:06:39 +0300

    HomeWork27

---
+++ HomeWork27/HomeWork27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        int numberToSquare = 10;
+        Task<int> task = CalculateAsync(numberToSquare);
+
+        Console.WriteLine("Task started");
+
+        
+        int result = await task;
+
+        Console.WriteLine("Result: " + result);
+    }
+
+    static async Task<int> CalculateAsync(int number)
+    {
+     
+        await Task.Delay(1000); 
+
+        return number * number;
+    }
+}
