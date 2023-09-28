9f56b0c0f66d5e6b390892422036966c7fb28a54
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-09-28 20:33:59 +0300

    HomeWork28

---
+++ HomeWork28/HomeWork28/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int arraySize = 100000000;
+        int[] numbers = Enumerable.Range(1, arraySize).ToArray();
+
+
+        int numberOfThreads = Environment.ProcessorCount;
+
+        if (args.Length > 0 && int.TryParse(args[0], out int userThreadCount) && userThreadCount > 0)
+        {
+            numberOfThreads = Math.Min(numberOfThreads, userThreadCount);
+        }
+
+        Console.WriteLine($"Using {numberOfThreads} threads for PLINQ processing.");
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        var result = numbers.AsParallel()
+                            .WithDegreeOfParallelism(numberOfThreads)
+                            .Where(x => x % 2 == 0)
+                            .Select(x => x * x)
+                            .ToList();
+
+        stopwatch.Stop();
+
+        Console.WriteLine("Result count: " + result.Count);
+        Console.WriteLine("Time taken: " + stopwatch.ElapsedMilliseconds + "ms");
+    }
+}
