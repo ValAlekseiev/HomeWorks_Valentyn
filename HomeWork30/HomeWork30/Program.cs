daace8b55a3892472974ebb8993234409e60bfbd
ozone106 <104835015+ozone106@users.noreply.github.com>
2023-10-05 16:35:14 +0300

    HomeWork30

---
+++ HomeWork30/HomeWork30/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class MyObject
+{
+    public void Run()
+    {
+        Console.Write("Enter the number of strings: ");
+        int numStrings = int.Parse(Console.ReadLine());
+
+        string[] strings = new string[numStrings];
+        for (int i = 0; i < numStrings; i++)
+        {
+            Console.Write($"Enter string #{i + 1}: ");
+            strings[i] = Console.ReadLine();
+        }
+
+        MyOtherObject otherObj = new MyOtherObject(strings);
+
+        otherObj.Dispose();
+    }
+}
+
+class MyOtherObject : IDisposable
+{
+    private string[] data;
+
+    public MyOtherObject(string[] strings)
+    {
+        data = strings;
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Number of strings: {data.Length}");
+        Console.WriteLine("Strings:");
+        foreach (string str in data)
+        {
+            Console.WriteLine(str);
+        }
+
+        
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        
+        Console.WriteLine("MyOtherObject was disposed.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyObject obj = new MyObject();
+        obj.Run();
+    }
+}
